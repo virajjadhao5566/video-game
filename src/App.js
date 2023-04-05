f34cce14dae94e7691e4351296d1f2bebb6526c2
Viraj <virajjadhao85@gmail.com>
2023-04-05 22:38:49 +0530

    api fetching done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [gameTitle,setGameTitle] = useState("")
+  const searchGame = () =>{
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
+    .then((response) =>response.json())
+    .then((data)=>{
+      console.log(data)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "searchSection">
+        <h1>Search For a Game</h1>
+        <input 
+          type="text" 
+          placeholder='Minecraft...'
+          onChange={(event)=>{
+            setGameTitle(event.target.value)
+          }}
+        />
+        <button onClick={searchGame}>Search Game</button>
+      </div>
+      <div className = "dealSection">
+
+      </div>
     </div>
   );
 }
