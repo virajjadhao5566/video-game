16d6fd34841f2161b27225efc5522f628038f63d
Viraj <virajjadhao85@gmail.com>
2023-04-07 16:23:54 +0530

    functionalities completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState} from 'react';
+import useSWR from 'swr'
+
+const fetcher = (...args) =>fetch(...args).then((response)=>response.json())
+
 function App() {
   const [gameTitle,setGameTitle] = useState("")
+  const [searchedGames,setSearchedGames] = useState([])
+
+  const {data,error} = useSWR('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3',fetcher)
   const searchGame = () =>{
-    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
     .then((response) =>response.json())
     .then((data)=>{
-      console.log(data)
+      setSearchedGames(data)
     })
   }
+  
   return (
     <div className="App">
       <div className = "searchSection">
@@ -21,9 +29,38 @@ function App() {
           }}
         />
         <button onClick={searchGame}>Search Game</button>
+        <div className='games'>
+          {
+            searchedGames.map((game,key)=>{
+              return(
+                <div className='game' key={key}>
+                  {game.external}
+                  <img src={game.thumb}/>
+                  {game.cheapest}
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
-      <div className = "dealSection">
-
+      <div className = "dealsSection">
+        <h1>Latest Deals</h1>
+        {
+          <div className='games'>
+            {
+               data && data.map((game,key)=>{
+                return(
+                  <div className='game' id='deals' key={key}>
+                    <h3>{game.title}</h3>
+                    <p>Normal Price:{game.normalPrice}</p>
+                    <p>Deal Price:{game.salePrice}</p>
+                    <h3>You Save:{game.savings.substr(0,2)}%</h3>
+                  </div>
+                )
+              })
+            }
+          </div>
+        }
       </div>
     </div>
   );
